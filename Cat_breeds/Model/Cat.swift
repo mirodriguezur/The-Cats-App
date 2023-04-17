e483a8e65c0df8849f914eb7c06d768b6d18c51e
Michael Rodriguez <miarodriguezur@unal.edu.co>
2023-04-16 23:19:20 -0500

    The model has been added

---
+++ Cat_breeds/Model/Cat.swift
@@ -0,0 +1,16 @@
+//
+//  Cat.swift
+//  Cat_breeds
+//
+//  Created by Michael Alexander Rodriguez Urbina on 16/04/23.
+//
+
+import Foundation
+
+public struct Cat {
+    let breedName: String
+    let origin: String
+    let affectionLevel: Int
+    let intelligence: Int
+    let imageURL: String
+}
